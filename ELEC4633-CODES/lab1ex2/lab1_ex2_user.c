33dc67e93ee1a061e73aed245cc1842839a370ae
Anthony Leung <jyanthonyleung@gmail.com>
2020-03-05 19:50:21 +1100

    Update lab1_ex2_user.c
    added pipe opener in user file

--- ELEC4633-CODES/lab1ex2/lab1_ex2_user.c
+++ ELEC4633-CODES/lab1ex2/lab1_ex2_user.c
@@ -3,17 +3,30 @@
 #include <rtai.h>
 #include <rtai_nam2num.h>
 #include <rtai_shm.h>
-
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <signal.h>
 
 int main(void)
 {
   /* Add code */
   int *udata;
   int count;
+  int FIFOcount;
   udata = rtai_malloc(nam2num("count"), sizeof(int));
+  // Open pipe as read only - return error message if failed
+  if((FIFOcount = open("/dev/rtf0", O_RDONLY))<0){
+    fprintf(stderr,"Error opening FIFO pipe");
+  }
+
   while(){
+    usleep(1)                                 // sleep for 1us because of context switching
     count = *udata
-    printf("\n %d", count);
+    printf("\nShared Memory: %d", count);
+    read(FIFOcount, &count, sizeof(count));   // Read pipe and save value into 'count' with sizeof 'count'
+    printf("\tFIFO buffer: %d", count);
   }
   rtai_free(nam2num("counter"));
   return 0;
